91432f6d78d154cbd7238976bc630f8198c0e87e
Abhijit Chatterjee <61982@aib.ie>
2020-01-29 14:44:20 +0000

    Added nodemon, koa-router, koa-json

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const http = require('http');
 const https = require('https');
 const Koa = require('koa');
-const app = new Koa()
+const KoaRouter = require('koa-router');
+const json = require('koa-json');
+
+const app = new Koa();
+const router = new KoaRouter();
 
 const port = process.env.PORT || 3000;
 
-app.use(ctx=> {
-    ctx.body = 'Here is a message';
-})
+//JSON Prettify Middleware
+app.use(json());
 
-http.createServer(app.callback().listen(3000));
+//Router Middleware
+app.use(router.routes()).use(router.allowedMethods());
+
+//Routers
+router.get('/test', async (ctx)=> {
+	ctx.body = {
+		msg: 'Hello Test'
+	}
+});
+
+http.createServer(app.listen(3000, () => {
+    console.log('Server Started!!');
+}));
 // https.createServer(app.callback().listen(3001));
